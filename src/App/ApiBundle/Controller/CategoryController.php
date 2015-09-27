361b53b598c311dd61e575f4ba9edb1accc666a5
jpsymfony <jpsymfony@free.fr>
2015-09-27 16:39:32 +0200

    added AbstractRepository for pagination and removed unused class import in CategoryController

--- src/App/ApiBundle/Controller/CategoryController.php
+++ src/App/ApiBundle/Controller/CategoryController.php
@@ -3,22 +3,17 @@
 namespace App\ApiBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\ParameterBag;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Validator\ConstraintViolationListInterface;
-
-
-use App\CoreBundle\Entity\Category;
 use FOS\RestBundle\Controller\FOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
 use Nelmio\ApiDocBundle\Annotation as Doc;
-
 use FOS\RestBundle\Request\ParamFetcherInterface;
+
+use App\CoreBundle\Entity\Category;
+use App\ApiBundle\Representation\Categories;
 
 
 /**
@@ -71,7 +66,7 @@ class CategoryController extends FOSRestController
             $paramFetcher->get('offset')
         );
         
-        return new \App\ApiBundle\Representation\Categories($categories);
+        return new Categories($categories);
     }
     
     /**
--- src/App/CoreBundle/Repository/AbstractRepository.php
+++ src/App/CoreBundle/Repository/AbstractRepository.php
@@ -1,5 +1,25 @@
 <?php
 
-abstract class AbstractRepository {
-    
+namespace Smoovio\Bundle\CoreBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\QueryBuilder;
+use Pagerfanta\Adapter\DoctrineORMAdapter;
+use Pagerfanta\Pagerfanta;
+
+abstract class AbstractRepository extends EntityRepository
+{
+    protected function paginate(QueryBuilder $qb, $limit = 20, $offset = 0)
+    {
+        $limit = (int) $limit;
+        if (0 === $limit) {
+            throw new \LogicException('$limit must be greater than 0.');
+        }
+
+        $pager = new Pagerfanta(new DoctrineORMAdapter($qb));
+        $pager->setCurrentPage(ceil(($offset + 1) / $limit));
+        $pager->setMaxPerPage((int) $limit);
+
+        return $pager;
+    }
 }
