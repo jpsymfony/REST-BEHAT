c3c23b743c2907803460c6f515bbeb933cc9288d
jpsymfony <jpsymfony@free.fr>
2015-10-08 16:26:24 +0200

    removed slugger to use doctrine one instead

--- src/App/CoreBundle/Entity/Manager/CategoryManager.php
+++ src/App/CoreBundle/Entity/Manager/CategoryManager.php
@@ -2,25 +2,17 @@
 
 namespace App\CoreBundle\Entity\Manager;
 
-use Cocur\Slugify\Slugify;
 use App\CoreBundle\Entity\Category;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class CategoryManager
 {
-    private $slugger;
-
-    public function __construct(Slugify $slugger, ObjectManager $em) {
-        $this->slugger = $slugger;
+    public function __construct(ObjectManager $em) {
         $this->em = $em;
     }
 
     public function update(Category $category, Category $apiCategory)
     {
-        if (!$apiCategory->getSlug()) {
-            $apiCategory->setSlug($this->slugger->slugify($category->getTitle()));
-        }
-
         $category->update($apiCategory);
 
         $this->em->flush();
@@ -28,10 +20,6 @@ class CategoryManager
 
     public function save(Category $category)
     {
-        if (!$category->getSlug()) {
-            $category->setSlug($this->slugger->slugify($category->getTitle()));
-        }
-
         $this->em->persist($category);
         $this->em->flush();
     }
